4b92ba8a282957e027165705984adf4bf7099884
PhilMinecraft <67897800+PhilMinecraft@users.noreply.github.com>
2021-02-19 04:54:21 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function openSlideMenu(){
+  document.getElementById('menu').style.marginLeft = '250px';
+  document.getElementById('content').style.marginLeft = '0';
+}
+function closeSlideMenu(){
+  document.getElementById('menu').style.marginLeft = '0';
+  document.getElementById('content').style.marginLeft = '0';
+}
